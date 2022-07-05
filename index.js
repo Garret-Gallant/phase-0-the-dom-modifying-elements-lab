e4b319b31b382cf5feeda4aebc3815d7de1bec76
Garret Gallant <gallanttechnological@gmail.com>
2022-07-05 11:45:05 -0600

    commit tree 49d1c6e617ef319067b4f98b4d0dd956dbf8ac72

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 document.querySelector('main').remove();
 
 const newHeader = document.querySelector('h1');
+
+function modNewHeader(newHeader){
+    document.getElementById(`victory`).textContent = newHeader
+}
+
+modNewHeader(`Garret is the champion!`);
